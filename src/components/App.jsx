b7145da6b7286cf4a8c10988c1eb9f35d94ee953
artemT82 <ar.tymchenko@gmail.com>
2017-05-31 23:54:25 +0300

    Add ability to leave parking

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,19 +2,25 @@ import React, { Component } from 'react';
 
 import config from '../config.js';
 
-import Parking          from './Parking.jsx';
-import JoinParkingModal from './JoinParkingModal.jsx';
+import Parking           from './Parking.jsx';;
+import JoinParkingModal  from './JoinParkingModal.jsx';
+import LeaveParkingModal from './LeaveParkingModal.jsx';
 
 import styles from './App.less';
 
 export default class App extends Component {
     state = {
         isJoinParkingModalShown: false,
+        isLeaveParkingModalShown: false,
         carsAmount: { truck: 0, disabled: 0, sedan: 0 }
     };
 
     handleJoinParkingBtnClick = () => {
         this.setState({ isJoinParkingModalShown: true });
+    };
+
+    handleLeaveParkingBtnClick = () => {
+        this.setState({ isLeaveParkingModalShown: true });
     };
 
     handleCarJoining = (carType) => {
@@ -24,6 +30,17 @@ export default class App extends Component {
 
         this.setState({
             isJoinParkingModalShown: false,
+            carsAmount: carsAmountUpdatedState
+        });
+    };
+
+    handleCarLeaving = (carType) => {
+        const carsAmountUpdatedState = this.state.carsAmount;
+
+        carsAmountUpdatedState[carType] = this.state.carsAmount[carType] - 1;
+
+        this.setState({
+            isLeaveParkingModalShown: false,
             carsAmount: carsAmountUpdatedState
         });
     };
@@ -58,6 +75,18 @@ export default class App extends Component {
         return this.getFreePlaces() > 0;
     };
 
+    checkCanSedanLeaveParking = () => {
+        return this.state.carsAmount.sedan > 0;
+    };
+
+    checkCanTruckLeaveParking = () => {
+        return this.state.carsAmount.truck > 0;
+    };
+
+    checkCanDisabledLeaveParking = () => {
+        return this.state.carsAmount.disabled > 0;
+    };
+
     render() {
         const {
             truksPlacesMaximum,
@@ -65,13 +94,20 @@ export default class App extends Component {
             totalPlaces
         } = config;
 
-        const canSedanJoingParking = this.checkCanSedanJoinParking();
-        const canTruckJoingParking = this.checkCanTruckJoinParking();
-        const canDisabledJoingParking = this.checkCanDisabledJoinParking();
+        const canSedanJoinParking = this.checkCanSedanJoinParking();
+        const canTruckJoinParking = this.checkCanTruckJoinParking();
+        const canDisabledJoinParking = this.checkCanDisabledJoinParking();
+
+        const canSedanLeaveParking = this.checkCanSedanLeaveParking();
+        const canTruckLeaveParking = this.checkCanTruckLeaveParking();
+        const canDisabledLeaveParking = this.checkCanDisabledLeaveParking();
+
+        console.log('canTruckLeaveParking', canTruckLeaveParking);
 
         return (
             <div className={styles.app} id='app'>
                 <div onClick={this.handleJoinParkingBtnClick}>Join parking</div>
+                <div onClick={this.handleLeaveParkingBtnClick}>Leave parking</div>
                 <Parking
                     truksPlacesMaximum={truksPlacesMaximum}
                     disabledPlacesMaximum={disabledPlacesMaximum}
@@ -83,10 +119,22 @@ export default class App extends Component {
                     this.state.isJoinParkingModalShown
                     ?
                         <JoinParkingModal
-                            canSedanJoingParking={canSedanJoingParking}
-                            canTruckJoingParking={canTruckJoingParking}
-                            canDisabledJoingParking={canDisabledJoingParking}
+                            canSedanJoinParking={canSedanJoinParking}
+                            canTruckJoinParking={canTruckJoinParking}
+                            canDisabledJoinParking={canDisabledJoinParking}
                             onCarTypeSelect={this.handleCarJoining}
+                        />
+                    :
+                        null
+                }
+                {
+                    this.state.isLeaveParkingModalShown
+                    ?
+                        <LeaveParkingModal
+                            canSedanLeaveParking={canSedanLeaveParking}
+                            canTruckLeaveParking={canTruckLeaveParking}
+                            canDisabledLeaveParking={canDisabledLeaveParking}
+                            onCarTypeSelect={this.handleCarLeaving}
                         />
                     :
                         null
--- src/components/LeaveParkingModal.jsx
+++ src/components/LeaveParkingModal.jsx
@@ -3,30 +3,30 @@ import cx                              from 'classnames';
 
 import * as labels from '../constants/Labels.js';
 
-import styles from './JoinParkingModal.less';
+import styles from './LeaveParkingModal.less';
 
-export default class JoinParkingModal extends Component {
+export default class LeaveParkingModal extends Component {
     static propTypes = {
-        canTruckJoinParking: PropTypes.bool.isRequired,
-        canSedanJoinParking: PropTypes.bool.isRequired,
-        canDisabledJoinParking: PropTypes.bool.isRequired,
+        canTruckLeaveParking: PropTypes.bool.isRequired,
+        canSedanLeaveParking: PropTypes.bool.isRequired,
+        canDisabledLeaveParking: PropTypes.bool.isRequired,
         onCarTypeSelect: PropTypes.func.isRequired
     };
 
     render() {
         const truckBtnClasses = cx(styles.carTypeBtn, {
-            [styles.carTypeBtnDisabled]: !this.props.canTruckJoinParking
+            [styles.carTypeBtnDisabled]: !this.props.canTruckLeaveParking
         });
         const sedanBtnClasses = cx(styles.carTypeBtn, {
-            [styles.carTypeBtnDisabled]: !this.props.canSedanJoinParking
+            [styles.carTypeBtnDisabled]: !this.props.canSedanLeaveParking
         });
         const disabledBtnClasses = cx(styles.carTypeBtn, {
-            [styles.carTypeBtnDisabled]: !this.props.canDisabledJoinParking
+            [styles.carTypeBtnDisabled]: !this.props.canDisabledLeaveParking
         });
 
         return (
             <div className={styles.modal}>
-                <h3>What type of car do you have?</h3>
+                <h3>What type of car have you left here?</h3>
                 <div
                     className={truckBtnClasses}
                     onClick={this.props.onCarTypeSelect.bind(null, labels.TRUCK_CAR_LABEL)}
